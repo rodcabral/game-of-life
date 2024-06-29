e984bfc9d61875d1b37742542508af28d9391c12
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2024-06-29 00:44:13 -0300

    print generation

--- life.c
+++ life.c
@@ -5,14 +5,30 @@
 #define ROW 10
 #define COL 10
 
-int grid[ROW][COL];
+int grid[ROW][COL] = {0};
 
 void init_state();
 int count_nbh();
 void next_gen();
+void print_gen();
 
 int main(void) {
+    srand(time(NULL));
 
+    print_gen();
 
     return 0;
+}
+
+void print_gen() {
+    for(int i = 0; i < ROW; ++i) {
+        for(int j = 0; j < COL; j++) {
+            if(grid[i][j] == 1) {
+                printf("0");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
 }
