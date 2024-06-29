78849c3dd36516c26b43377fafcc0196079890fb
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2024-06-29 01:28:31 -0300

    implement rules and generates the next gen

--- life.c
+++ life.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
-#define ROW 10
-#define COL 10
+#define ROW 15
+#define COL 15
 
 int grid[ROW][COL] = {0};
 
 void init_state();
-int count_nbh();
+int count_nbh(int row, int col);
 void next_gen();
 void print_gen();
 
 int main(void) {
     srand(time(NULL));
 
+    system("clear");
     init_state();
-    print_gen();
+    while(1) {
+        print_gen();
+        next_gen();
+        printf("\e[1H");
+        sleep(1);
+    }
 
     return 0;
 }
@@ -39,5 +46,37 @@ void print_gen() {
             }
         }
         printf("\n");
+    }
+}
+
+int count_nbh(int r, int c) {
+    int nbh = 0;
+
+    for(int i = r - 1; i <= r + 1; ++i) {
+        for(int j = c - 1; j <= c + 1; ++j) {
+            if((i == r && j == c) || (i >= ROW || j >= COL) || (i < 0 || j < 0)) continue;
+             
+            if(grid[i][j] == 1) {
+                nbh++;
+            }
+        }
+    }
+
+    return nbh;
+}
+
+void next_gen() {
+    for(int i = 0; i < ROW; ++i) {
+        for(int j = 0; j < COL; j++) {
+            int nbh = count_nbh(i, j);
+
+            if(grid[i][j] == 1 && (nbh == 2 || nbh == 3)) {
+                grid[i][j] = 1;
+            } else if(grid[i][j] == 0 && nbh == 3) { 
+                grid[i][j] = 1;
+            } else {
+                grid[i][j] = 0;
+            }
+        }
     }
 }
