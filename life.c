dbf4e6bcea5075be7244977a7bcbe7111fac28a5
Rodrigo Cabral <37848619+rodcabral@users.noreply.github.com>
2024-06-29 00:39:00 -0300

    initial commit

---
+++ life.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ROW 10
+#define COL 10
+
+int grid[ROW][COL];
+
+void init_state();
+int count_nbh();
+void next_gen();
+
+int main(void) {
+
+
+    return 0;
+}
